97803b9857d43a0a40ddbafbd5a59fb727c7ba4d
Yurii <y.tsyhykal@gmail.com>
2025-01-29 22:21:16 +0100

    add base routes/header/layout

---
+++ src/components/pages/NanniesPage.jsx
@@ -0,0 +1,5 @@
+const NanniesPage = () => {
+  return <>Nannies</>;
+};
+
+export default NanniesPage;
---
+++ src/components/pages/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+  return <>Not Found</>;
+};
+
+export default NotFoundPage;
