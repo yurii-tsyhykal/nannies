c4d5f1b430bcac429fb48bbe6914b806b14102c3
Yurii <y.tsyhykal@gmail.com>
2025-02-06 00:08:32 +0100

    added avatar status online

--- src/components/NanniesListItem/NanniesListItem.jsx
+++ src/components/NanniesListItem/NanniesListItem.jsx
@@ -1,7 +1,19 @@
 import css from './NanniesListItem.module.css';
 
-const NanniesListItem = () => {
-  return <>item</>;
+const NanniesListItem = ({ nanny }) => {
+  return (
+    <>
+      <div className={css.nannyAvatar}>
+        <img
+          className={css.avatar}
+          src={nanny.avatar_url}
+          alt="Nanny’s avatar"
+          width={96}
+          height={96}
+        />
+      </div>
+    </>
+  );
 };
 
 export default NanniesListItem;
