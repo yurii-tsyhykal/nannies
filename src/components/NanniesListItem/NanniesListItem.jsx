2dfd99e31c05995bb9d19231958bc1585b3c254e
Yurii <y.tsyhykal@gmail.com>
2025-02-09 15:12:33 +0100

    styled card, added reviews hide/visible logic

--- src/components/NanniesListItem/NanniesListItem.jsx
+++ src/components/NanniesListItem/NanniesListItem.jsx
@@ -3,8 +3,11 @@ import { IoLocationOutline } from 'react-icons/io5';
 import { FaStar } from 'react-icons/fa6';
 import ageCalculate from '../../utils/ageCalc';
 import charactersToUpper from '../../utils/CharactersToUpper';
+import { useState } from 'react';
+import Reviews from '../Reviews/Reviews';
 
 const NanniesListItem = ({ nanny }) => {
+  const [visibleReviews, setVisibleReviews] = useState(false);
   const age = ageCalculate(nanny.birthday);
   const characters = charactersToUpper(nanny.characters);
 
@@ -48,13 +51,36 @@ const NanniesListItem = ({ nanny }) => {
           <span style={{ color: '#38cd3e' }}>{nanny.price_per_hour}$</span>
         </p>
       </div>
-      <ul>
-        <li style={{ color: 'black' }}>Age: {age}</li>
-        <li style={{ color: 'black' }}>Experience: {nanny.experience}</li>
-        <li style={{ color: 'black' }}>Kids Age: {nanny.kids_age}</li>
-        <li style={{ color: 'black' }}>Characters: {characters}</li>
-        <li style={{ color: 'black' }}>Education: {nanny.education}</li>
+      <ul className={css.profileList}>
+        <li>
+          Age: <span className={css.profileValue}>{age}</span>
+        </li>
+        <li>
+          Experience:{' '}
+          <span className={css.profileValue}>{nanny.experience}</span>
+        </li>
+        <li>
+          Kids Age: <span className={css.profileValue}>{nanny.kids_age}</span>
+        </li>
+        <li>
+          Characters: <span className={css.profileValue}>{characters}</span>
+        </li>
+        <li>
+          Education: <span className={css.profileValue}>{nanny.education}</span>
+        </li>
       </ul>
+      <p className={css.about}>{nanny.about}</p>
+      {!visibleReviews ? (
+        <button
+          type="button"
+          style={{ color: 'black' }}
+          onClick={() => setVisibleReviews(true)}
+        >
+          Read more
+        </button>
+      ) : (
+        <Reviews reviews={nanny.reviews} />
+      )}
     </>
   );
 };
