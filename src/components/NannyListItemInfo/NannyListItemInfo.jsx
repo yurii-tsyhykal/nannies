d5a4b0de4efb5db0bc6136bc64973ed65bc22b7f
Yurii <y.tsyhykal@gmail.com>
2025-05-12 20:28:44 +0200

    reloc favbtn add styles for nannisprof

--- src/components/NannyListItemInfo/NannyListItemInfo.jsx
+++ src/components/NannyListItemInfo/NannyListItemInfo.jsx
@@ -3,25 +3,27 @@ import css from './NannyListItemInfo.module.css';
 
 const NannyListItemInfo = ({ nanny }) => {
   return (
-    <ul className={css.info}>
-      <li className={css.infoListItems}>
-        <svg className={css.location} width={16} height={16}>
-          <use href={'/images/sprite.svg#location'}></use>
-        </svg>
-        <span>{nanny.location}</span>
-      </li>
-      <li className={css.infoListItems}>
-        <svg className={css.star} width={16} height={16}>
-          <use href={'/images/sprite.svg#star'}></use>
-        </svg>
-        <span>Rating: {nanny.rating}</span>
-      </li>
-      <li className={css.infoListItems}>
-        <span>Price / 1 hour: </span>
-        <span className={css.priceText}>&nbsp;{nanny.price_per_hour}$</span>
-      </li>
+    <>
+      <ul className={css.info}>
+        <li className={css.infoListItems}>
+          <svg className={css.location} width={16} height={16}>
+            <use href={'/images/sprite.svg#location'}></use>
+          </svg>
+          <span>{nanny.location}</span>
+        </li>
+        <li className={css.infoListItems}>
+          <svg className={css.star} width={16} height={16}>
+            <use href={'/images/sprite.svg#star'}></use>
+          </svg>
+          <span>Rating: {nanny.rating}</span>
+        </li>
+        <li className={css.infoListItems}>
+          <span>Price / 1 hour: </span>
+          <span className={css.priceText}>&nbsp;{nanny.price_per_hour}$</span>
+        </li>
+      </ul>
       <FavoritesButton nanny={nanny} />
-    </ul>
+    </>
   );
 };
 
